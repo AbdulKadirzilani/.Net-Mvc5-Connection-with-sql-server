b154f8336bef14f00aff7db570b5cc5c3af67645
Abdul kader <abdulkadir14015@gmail.com>
2019-10-16 03:11:49 +0600

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tuto7.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc; 
+using System.Data.SqlClient;
+
+namespace Tuto7.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+
+        public string SaveStd(Student student)
+        {
+          
+              SqlConnection connection = new SqlConnection(@"Server=DESKTOP-059A10P\SQLEXPRESS;Database=ASPCRUD;Integrated Security=true;");
+
+            connection.Open();
+            string query = "INSERT INTO Contact2(Name, Mobile)VALUES('" + student.Name+"','"+student.Mobile+"')";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            int rowCount = cmd.ExecuteNonQuery();
+            if (rowCount > 0)
+            {
+
+
+                return "save data success";
+            }
+
+                return "failed";
+
+
+            
+
+        }
+    }
+
+}
